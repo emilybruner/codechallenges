52eda221e2b3eb8eb41ff26ca441c0baefa4ae58
Emily Bruner <bruner.emilym@gmail.com>
2020-09-07 16:46:51 -0400

    palindrome checker

---
+++ palindromechecker.js
@@ -0,0 +1,15 @@
+
+
+let pcheck = (s) => {
+    let reverseString = (s) => {
+    return s.split('').reverse().join('')
+  } 
+    
+    if (s === reverseString(s)) {
+      return true
+    } else {
+      return false
+    }
+  }
+  
+  
